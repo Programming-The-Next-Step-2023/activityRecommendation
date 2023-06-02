d8679ab7d96559cc44beb936c5261bbc5aceca1d
FleurKorzilius <94118712+FleurKorzilius@users.noreply.github.com>
2023-06-02 22:57:47 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+      name='activityRec',
+      version='1.0',
+      description='Self-Care Activity Recommender',
+      author='Fleur Korzilius',
+      author_email='f.f.a.korzilius@uva.nl',
+      url='',
+      packages=['activityRec'],
+     )
